5e5bf740304346553a1bfe5c24fea7562e843e01
Anz072 <rokasbalt02@gmail.com>
2023-11-23 22:27:14 +0200

    sign up componenet, form component, button component

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -2,17 +2,20 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../../sign-up-form/sign-up-form.component";
+import Button from "../../componentsMain/button/button.componenet";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef =  await createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>SignIn</button>
+      <Button onClick={logGoogleUser} buttonType={"google"}>SignIn</Button>
+      <SignUpForm/>
     </div>
   );
 };
---
+++ src/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
+import FormInput from "../components/componentsMain/form-input/form-input.component";
+import './sign-up-form.styles.scss'
+import Button from "../components/componentsMain/button/button.componenet";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  passowrd: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, passowrd, confirmPassword } = formFields;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (passowrd !== confirmPassword) return;
+
+    try {
+      const authUser = await createAuthUserWithEmailAndPassword(
+        email,
+        passowrd
+      );
+      const userDocRef = await createUserDocumentFromAuth(authUser.user, {
+        displayName,
+      });
+
+      setFormFields(defaultFormFields);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+        <h2>Dont have an account?</h2>
+      <span>Sign Up loser</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Passowrd"
+          type="password"
+          required
+          onChange={handleChange}
+          name="passowrd"
+          value={passowrd}
+        />
+
+        <FormInput
+          label="Confirm Passowrd"
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+        <Button type="submit">Register</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
